acaae3cb0b2382fa403e118764a61025b9b0568f
Yutao Zhou <91305696+Yutao-Zhou@users.noreply.github.com>
2022-11-13 12:07:09 -0500

    Add Title and Legend

--- HW3/Task2/2.2.js
+++ HW3/Task2/2.2.js
@@ -50,7 +50,7 @@
         height = 410,
         radius = (Math.min(width, height)-50) / 3;
 
-        var color = d3.scaleOrdinal(d3.schemeCategory10);
+        var color = d3.scaleOrdinal(d3.schemeCategory20);
 
         var arc = d3.arc()
           .outerRadius(radius - 10)
@@ -71,11 +71,22 @@
 
         svg.append("text")
             .attr("x", 0)             
-            .attr("y", width / 2)
+            .attr("y", -width / 2 + 40)
             .attr("text-anchor", "middle")  
             .style("font-size", "16px") 
-            .style("text-decoration", "underline")  
+            .style("font-weight", "bold")
             .text("Pie Chart of distribution of the model years");
+
+        var counter = 0
+        var c = ["rgb(31, 119, 180)","rgb(174, 199, 232)","rgb(255, 127, 14)","rgb(255, 187, 120)","rgb(44, 160, 44)",
+          "rgb(152, 223, 138)","rgb(214, 39, 40)","rgb(255, 152, 150)","rgb(148, 103, 189)","rgb(197, 176, 213)",
+          "rgb(140, 86, 75)","rgb(196, 156, 148)","rgb(227, 119, 194)"]
+        var name = ["70", "71", "72", "73", "74", "75","76", "77", "78", "79", "80", "81","82"]
+        pieData.forEach(function(d) {
+          svg.append("circle").attr("cx",width / 2 - 40).attr("cy",-width / 2 + 50 +counter * 15).attr("r", 6).style("fill", c[counter])
+          svg.append("text").attr("x", width / 2 - 30).attr("y", -width / 2 + 50 +counter * 15).text(name[counter]).style("font-size", "15px").attr("alignment-baseline","middle")
+          counter = counter + 1
+        });
 
         //total count for percentage calculation
         var total = d3.sum(pieData, function(d) { 
@@ -102,8 +113,8 @@
         g.append("text")
           .attr("transform", function(d) {
             var _d = arc.centroid(d);
-            _d[0] *= 2.7;
-            _d[1] *= 2.7;
+            _d[0] *= 2.4;
+            _d[1] *= 2.4;
             return "translate(" + _d + ")" 
 
           })
@@ -116,8 +127,8 @@
         g.append("text")
           .attr("transform", function(d) {
             var _d = arc.centroid(d);
-            _d[0] *= 2.7;
-            _d[1] *= 2.7;
+            _d[0] *= 2.4;
+            _d[1] *= 2.4;
             return "translate(" + _d + ")" 
 
           })
@@ -157,6 +168,17 @@
           .append("g")
             .attr("transform",
                   "translate(" + margin.left + "," + margin.top + ")");
+
+            svg.append("text")
+              .attr("x", width / 2)             
+              .attr("y", -8)
+              .attr("text-anchor", "middle")  
+              .style("font-size", "16px")
+              .style("font-weight", "bold")
+              .text("Line Graph of Model Year vs Total Number of Cylinders");
+
+            svg.append("line").attr("x1", width - 110).attr("y1", 10).attr("x2", width - 80).attr("y2", 10).style("stroke", "steelblue").style("stroke-width", 3)
+            svg.append("text").attr("x", width - 70).attr("y", 10).text("Precipitation").style("font-size", "15px").attr("alignment-baseline","middle")
         
         var parseTime = d3.timeParse("%y");
         lineGraph.forEach(function(d) {d.key = parseTime(d.key)})
@@ -235,6 +257,17 @@
           .append("g")
             .attr("transform",
                   "translate(" + margin.left + "," + margin.top + ")");
+
+            svg.append("text")
+              .attr("x", width / 2)             
+              .attr("y", -8)
+              .attr("text-anchor", "middle")  
+              .style("font-size", "16px")
+              .style("font-weight", "bold")
+              .text("Histogram of Acceleration Distribution With Bin Of 10");
+
+            svg.append("circle").attr("cx",width - 100).attr("cy",10).attr("r", 6).style("fill", "#69b3a2")
+            svg.append("text").attr("x", width - 90).attr("y", 10).text("Wind Speed").style("font-size", "15px").attr("alignment-baseline","middle")
 
             var x = d3.scaleLinear()
               .domain([6,28])
